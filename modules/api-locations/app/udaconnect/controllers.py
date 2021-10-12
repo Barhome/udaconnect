0601b080e14738c0e0a94197d031fdd90c67f998
Barhome <ielsamoly@gmail.com>
2021-10-12 16:27:41 +0200

    refactor: Seprating loctions services

--- modules/api-locations/app/udaconnect/controllers.py
+++ modules/api-locations/app/udaconnect/controllers.py
@@ -6,7 +6,7 @@ from app.udaconnect.schemas import (
     LocationSchema,
     PersonSchema,
 )
-from app.udaconnect.services import ConnectionService, LocationService, PersonService
+from app.udaconnect.services import  LocationService
 from flask import request
 from flask_accepts import accepts, responds
 from flask_restx import Namespace, Resource
@@ -21,6 +21,19 @@ api = Namespace("UdaConnect", description="Connections via geolocation.")  # noq
 
 
 @api.route("/locations")
+class PersonsResource(Resource):
+    @accepts(schema=LocationSchema)
+    @responds(schema=LocationSchema)
+    def post(self) -> Location:
+        payload = request.get_json()
+        new_location: Location = LocationService.create(payload)
+        return new_location
+
+    @responds(schema=LocationSchema, many=True)
+    def get(self) -> List[Location]:
+        persons: List[Location] = LocationService.retrieve_all()
+        return persons
+
 @api.route("/locations/<location_id>")
 @api.param("location_id", "Unique ID for a given Location", _in="query")
 class LocationResource(Resource):
@@ -36,48 +49,3 @@ class LocationResource(Resource):
         location: Location = LocationService.retrieve(location_id)
         return location
 
-
-@api.route("/persons")
-class PersonsResource(Resource):
-    @accepts(schema=PersonSchema)
-    @responds(schema=PersonSchema)
-    def post(self) -> Person:
-        payload = request.get_json()
-        new_person: Person = PersonService.create(payload)
-        return new_person
-
-    @responds(schema=PersonSchema, many=True)
-    def get(self) -> List[Person]:
-        persons: List[Person] = PersonService.retrieve_all()
-        return persons
-
-
-@api.route("/persons/<person_id>")
-@api.param("person_id", "Unique ID for a given Person", _in="query")
-class PersonResource(Resource):
-    @responds(schema=PersonSchema)
-    def get(self, person_id) -> Person:
-        person: Person = PersonService.retrieve(person_id)
-        return person
-
-
-@api.route("/persons/<person_id>/connection")
-@api.param("start_date", "Lower bound of date range", _in="query")
-@api.param("end_date", "Upper bound of date range", _in="query")
-@api.param("distance", "Proximity to a given user in meters", _in="query")
-class ConnectionDataResource(Resource):
-    @responds(schema=ConnectionSchema, many=True)
-    def get(self, person_id) -> ConnectionSchema:
-        start_date: datetime = datetime.strptime(
-            request.args["start_date"], DATE_FORMAT
-        )
-        end_date: datetime = datetime.strptime(request.args["end_date"], DATE_FORMAT)
-        distance: Optional[int] = request.args.get("distance", 5)
-
-        results = ConnectionService.find_contacts(
-            person_id=person_id,
-            start_date=start_date,
-            end_date=end_date,
-            meters=distance,
-        )
-        return results
