5e026455e1c69e267fbd7abc695a15f91eb83efb
Barhome <ielsamoly@gmail.com>
2021-10-25 01:55:39 +0200

    fix: syntax

--- modules/api-locations/app/udaconnect/services.py
+++ modules/api-locations/app/udaconnect/services.py
@@ -48,9 +48,9 @@ class LocationService:
         for message in kafka_consumer:
             new_message= message.value
             new_location = Location()
-            new_location.person_id = new_message.person_id
-            new_location.creation_time = new_message.creation_time
-            new_location.coordinate = ST_Point(new_message.latitude, new_message.longitude)
+            new_location.person_id = new_message['person_id']
+            new_location.creation_time = new_message['creation_time']
+            new_location.coordinate = ST_Point(new_message['latitude'], new_message['longitude'])
             db.session.add(new_location)
             db.session.commit()
         return new_location
